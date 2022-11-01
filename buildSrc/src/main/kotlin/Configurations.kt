f61dfc22f5be9325b5bbb9293f5c1c0eba33cf97
skydoves <skydoves@naver.com>
2022-11-01 10:36:48 +0100

    Update version

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -4,7 +4,7 @@ object Configurations {
   const val minSdk = 21
   const val majorVersion = 1
   const val minorVersion = 0
-  const val patchVersion = 1
+  const val patchVersion = 2
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 2
+  const val versionCode = 3
 }
