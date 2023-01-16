c6837ea8724dff19c425796af9d60a57bc93da56
skydoves <skydoves@naver.com>
2023-01-16 18:04:20 +0900

    Update license date

--- app/src/main/kotlin/io/getstream/whatsappclone/navigation/WhatsAppNavigation.kt
+++ app/src/main/kotlin/io/getstream/whatsappclone/navigation/WhatsAppNavigation.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+ * Copyright 2023 Stream.IO, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
