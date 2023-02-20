a1cd4fe64ff7276a7f50f87862c1ccf693558671
skydoves <skydoves@naver.com>
2023-02-20 15:38:32 +0900

    Update baseline profiles

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,49 +1,49 @@
 plugins {
-    `kotlin-dsl`
+  `kotlin-dsl`
 }
 
 group = "io.getstream.whatsappclone.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
-    compileOnly(libs.android.gradlePlugin)
-    compileOnly(libs.kotlin.gradlePlugin)
-    compileOnly(libs.spotless.gradlePlugin)
+  compileOnly(libs.android.gradlePlugin)
+  compileOnly(libs.kotlin.gradlePlugin)
+  compileOnly(libs.spotless.gradlePlugin)
 }
 
 gradlePlugin {
-    plugins {
-        register("androidApplicationCompose") {
-            id = "getstream.android.application.compose"
-            implementationClass = "AndroidApplicationComposeConventionPlugin"
-        }
-        register("androidApplication") {
-            id = "getstream.android.application"
-            implementationClass = "AndroidApplicationConventionPlugin"
-        }
-        register("androidLibraryCompose") {
-            id = "getstream.android.library.compose"
-            implementationClass = "AndroidLibraryComposeConventionPlugin"
-        }
-        register("androidLibrary") {
-            id = "getstream.android.library"
-            implementationClass = "AndroidLibraryConventionPlugin"
-        }
-        register("androidFeature") {
-            id = "getstream.android.feature"
-            implementationClass = "AndroidFeatureConventionPlugin"
-        }
-      register("androidHilt") {
-        id = "getstream.android.hilt"
-        implementationClass = "AndroidHiltConventionPlugin"
-      }
-        register("spotless") {
-            id = "getstream.spotless"
-            implementationClass = "SpotlessConventionPlugin"
-        }
+  plugins {
+    register("androidApplicationCompose") {
+      id = "getstream.android.application.compose"
+      implementationClass = "AndroidApplicationComposeConventionPlugin"
     }
+    register("androidApplication") {
+      id = "getstream.android.application"
+      implementationClass = "AndroidApplicationConventionPlugin"
+    }
+    register("androidLibraryCompose") {
+      id = "getstream.android.library.compose"
+      implementationClass = "AndroidLibraryComposeConventionPlugin"
+    }
+    register("androidLibrary") {
+      id = "getstream.android.library"
+      implementationClass = "AndroidLibraryConventionPlugin"
+    }
+    register("androidFeature") {
+      id = "getstream.android.feature"
+      implementationClass = "AndroidFeatureConventionPlugin"
+    }
+    register("androidHilt") {
+      id = "getstream.android.hilt"
+      implementationClass = "AndroidHiltConventionPlugin"
+    }
+    register("spotless") {
+      id = "getstream.spotless"
+      implementationClass = "SpotlessConventionPlugin"
+    }
+  }
 }
