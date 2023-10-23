0c382185ca155f3e5c33a10fb395b8b62760cf09
skydoves <skydoves@naver.com>
2023-10-23 15:36:25 +0900

    Implement StreamVideoInitializer

---
+++ app/src/main/kotlin/io/getstream/whatsappclone/initializer/MainInitializer.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023 Stream.IO, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.whatsappclone.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import io.getstream.whatsappclone.chats.initializer.StreamChatInitializer
+import io.getstream.whatsappclone.video.initializer.StreamVideoInitializer
+
+class MainInitializer : Initializer<Unit> {
+
+  override fun create(context: Context) {
+  }
+
+  override fun dependencies(): List<Class<out Initializer<*>>> = listOf(
+    StreamChatInitializer::class.java,
+    StreamVideoInitializer::class.java
+  )
+}
---
+++ features/video/src/main/kotlin/io/getstream/whatsappclone/video/initializer/StreamVideoInitializer.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 Stream.IO, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.whatsappclone.video.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import io.getstream.video.android.core.StreamVideoBuilder
+import io.getstream.video.android.model.User
+import io.getstream.whatsappclone.network.Dispatcher
+import io.getstream.whatsappclone.network.WhatsAppDispatchers
+import io.getstream.whatsappclone.network.service.StreamVideoTokenService
+import io.getstream.whatsappclone.video.BuildConfig
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class StreamVideoInitializer : Initializer<Unit> {
+
+  @Inject
+  lateinit var streamVideoTokenService: StreamVideoTokenService
+
+  @Inject
+  @Dispatcher(WhatsAppDispatchers.IO)
+  lateinit var dispatcher: CoroutineDispatcher
+
+  private val coroutineScope = CoroutineScope(dispatcher)
+
+  override fun create(context: Context) {
+    StreamVideoEntryPoint.resolve(context)
+
+    val userId = "stream_user"
+    coroutineScope.launch {
+      val token = streamVideoTokenService.fetchToken(
+        userId = userId,
+        apiKey = BuildConfig.STREAM_API_KEY
+      ).getOrThrow().token
+
+      // initialize Stream Video SDK
+      StreamVideoBuilder(
+        context = context,
+        apiKey = BuildConfig.STREAM_API_KEY,
+        token = token,
+        user = User(
+          id = userId,
+          name = "Stream User",
+          image = "http://placekitten.com/200/300",
+          role = "admin"
+        )
+      ).build()
+    }
+  }
+
+  override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+}
