e6bb76452b9b95a952fb8887d3beec37921c6cec
skydoves <skydoves@naver.com>
2023-10-26 09:30:26 +0900

    Update version name to 1.1.0

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -2,8 +2,8 @@ object Configurations {
   const val targetSdk = 34
   const val minSdk = 24
   const val majorVersion = 1
-  const val minorVersion = 0
-  const val patchVersion = 4
+  const val minorVersion = 1
+  const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 5
+  const val versionCode = 6
 }
